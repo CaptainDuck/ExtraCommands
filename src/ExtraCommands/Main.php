37b485bdf1c3624b33eabb86b9abe518b6c0f695
CaptainDuck <jmdimayuga021@gmail.com>
2016-06-10 22:57:02 +0800

    Update Main.php

--- src/ExtraCommands/Main.php
+++ src/ExtraCommands/Main.php
@@ -73,7 +73,7 @@ class Main extends PluginBase implements Listener{
             case "info":
                 if($sender->hasPermission("ec.info")){
                     $sender->sendMessage(C::GRAY."Server Info");
-                    $sender->sendMessage(C::BLUE."Online:". count($this->getServer()->getOnlinePlayers() . "/" . $this->getServer()->getMaxPlayers()));
+                    $sender->sendMessage(C::BLUE."Online:". count($this->getServer()->getOnlinePlayers() . "/" . count($this->getServer()->getMaxPlayers()));
                     $sender->sendMessage(C::WHITE."Server IP: ". $this->getConfig()->get("ServerIP"));
                     $sender->sendMessage(C::WHITE."You're playing on ". $this->getConfig()->get("ServerName"). "!");  
                     return true;
